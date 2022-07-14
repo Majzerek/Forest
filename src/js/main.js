0eb2c458a6707bbeab61bebc3c0088b57846e682
Mariusz <majzermariusz@gmail.com>
2022-07-14 11:09:46 +0200

    mobilenav

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 const footerYear = document.querySelector('#footer-year')
+const hamburgerBtn = document.querySelector('.ti')
+const navMobile = document.querySelector('.nav-mobile')
+const navItems = document.querySelectorAll('.hide')
+
+const handleNav = () => {
+	navMobile.classList.toggle('active')
+}
+const hideNav = () => {
+	navItems.forEach.addEventListener('click', handleNav)
+}
+
+
+
+
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
@@ -6,3 +20,5 @@ const handleCurrentYear = () => {
 };
 
 handleCurrentYear();
+hamburgerBtn.addEventListener('click', handleNav)
+navItems.forEach.addEventListener('click', hideNav)
